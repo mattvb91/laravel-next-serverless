c4368928c4e751253f9cfb70b4636d79a069113c
Matthias von Bargen <matt@mavon.ie>
2021-04-01 12:29:41 +0200

    init backend

---
+++ frontend/pages/api/[...slug].js
@@ -0,0 +1,19 @@
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+// Create proxy instance outside of request handler function to avoid unnecessary re-creation
+const apiProxy = createProxyMiddleware({
+    target: process.env.API_TARGET,
+    changeOrigin: true,
+    pathRewrite: { [`^/api`]: '/api' },
+    secure: false,
+});
+
+export default function (req, res) {
+    apiProxy(req, res, (result) => {
+        if (result instanceof Error) {
+            throw result;
+        }
+
+        throw new Error(`Request '${req.url}' is not proxied! We should never reach here!`);
+    });
+};
