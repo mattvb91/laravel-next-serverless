134a0ad401979613fc5f28682c8da44f64ce688b
Matthias von Bargen <matt@mavon.ie>
2021-04-16 14:22:30 +0200

    db seed

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(1)->create([
+            'name'     => "Test Account",
+            'email'    => 'test@test.com',
+            'password' => Hash::make('test123'),
+        ]);
     }
 }
