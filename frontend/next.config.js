7416fd1aa6c75faf662ac53da65a80590a76ce89
Matthias von Bargen <matt@mavon.ie>
2021-09-11 13:13:37 +0100

    upgrade

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
-const withTM = require('next-transpile-modules');
-
-module.exports = withCSS(withSass(withTM({
-    future: {
-        webpack5: true,
-    },
-})));
