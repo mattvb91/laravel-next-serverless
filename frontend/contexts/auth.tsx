1c96e06847959453049ee851165cf20638a927d5
Matthias von Bargen <matt@mavon.ie>
2022-02-19 09:55:21 +0100

    fix order for auth

--- frontend/contexts/auth.tsx
+++ frontend/contexts/auth.tsx
@@ -95,7 +95,6 @@ export const AuthProvider = ({ children }) => {
 
         const response = await res.json()
         if (response.access_token) {
-            setIdentity(response)
             setCookie("token", response.access_token, {
                 path: "/",
                 maxAge: 3600, // Expires after 1hr
@@ -103,6 +102,8 @@ export const AuthProvider = ({ children }) => {
                 httpOnly: false
             })
             window.localStorage.setItem("canRefresh", "true")
+            setIdentity(response)
+
             router.push("/")
         }
     }
